6551cf52daf29b0e60b85031115d0a9ead00b460
BaselOmsha <90252817+BaselOmsha@users.noreply.github.com>
2022-02-16 22:35:25 +0200

    content added and updated

--- php/admin_remove.php
+++ php/admin_remove.php
@@ -1,24 +1,35 @@
 <?php
-mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
-$poistettava=isset($_GET["poistettava"]) ? $_GET["poistettava"] : 0;
-if (empty($poistettava)) {
-    header("Location:./listakirja.php");
-    exit;
-}
-try {
-    $connection=mysqli_connect("db", "root",
-        "password", "kirjakanta");
-} catch (Exception $e) {
-    header("Location:../html/yhteysvirhe.html");
-    exit;
-}
-$sql="delete from kirja where id=?";
-//Valmistellaan sql-lause
-$stmt=mysqli_prepare($connection, $sql);
-//Sijoitetaan muuttujat oikeisiin paikkoihin
-mysqli_stmt_bind_param($stmt, 'i', $poistettava);
-//Suoritetaan sql-lause
-mysqli_stmt_execute($stmt);
-mysqli_close($connection);
-header("Location:./listakirja.php")
+    mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
+    $deletable=isset($_GET["deletable"]) ? $_GET["deletable"] : 0;
+    $initials=parse_ini_file("../.ht.asetukset.ini");
+    if (empty($deletable)) {
+        header("Location:./admin.php");
+        exit;
+    }
+    try {
+        $connection=mysqli_connect($initials["databaseserver"],
+            $initials["username"],
+            $initials["password"],
+            $initials["database"]
+            );
+    } catch (Exception $e) {
+        header("Location:../html/connectionError.html");
+        exit;
+    }
+    $sql="delete from users where id=?";
+    //Valmistellaan sql-lause
+    $stmt=mysqli_prepare($connection, $sql);
+    //Sijoitetaan muuttujat oikeisiin paikkoihin
+    mysqli_stmt_bind_param($stmt, 'i', $deletable);
+    //Suoritetaan sql-lause
+    mysqli_stmt_execute($stmt);
+    mysqli_close($connection);
+    header("Refresh:3; url=./admin.php");
+    include "../html/admin_header.html";
+    echo "<div class='sec'>";
+    echo "<h1>The profile selected is being deleted...</h1><br>";
+    echo "<br>";
+    echo "<h1>Please wait!</h1>";
+    echo "</div>";
+    include "../html/admin_footer.html";
 ?>
