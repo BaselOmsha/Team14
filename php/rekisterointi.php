5bda328a970da94f3b282e6f7cfdc8992b224522
BaselOmsha <90252817+BaselOmsha@users.noreply.github.com>
2022-02-17 12:30:54 +0200

    updated

--- php/rekisterointi.php
+++ php/rekisterointi.php
@@ -1,5 +1,6 @@
 
 <?php
+$initials=parse_ini_file("../.ht.asetukset.ini");
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 $json=isset($_POST["users"]) ? $_POST["users"] : "";
 
@@ -9,7 +10,7 @@ if (!($users=tarkistaJson($json))){
 }
 
 try{
-    $initials=parse_ini_file("../.ht.asetukset.ini");
+   
     $yhteys=mysqli_connect($initials["databaseserver"],
         $initials["username"],
         $initials["password"],
@@ -17,19 +18,20 @@ try{
         );
     
 } catch (Exception $e) {
-    header("Location:../html/yhteysvirhe.html");
+    header("Location:../html/connectionError.html");
     exit;
 }
 
 //Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
 //joihin laitetaan muuttujien arvoja
-$sql="insert into users(fname, lname, email, uname, paswd) values(?,?,?,?, SHA2(?, 256))";//sama kuin SHA2(?, 0)
+$sql="insert into users(id, fname, lname, email, paswd, descrip, uname) values(?,?,?, SHA2(?, 256), ?, ?)";//sama kuin SHA2(?, 0)
 try{
     $stmt=mysqli_prepare($yhteys, $sql);
-    mysqli_stmt_bind_param($stmt, 'sssss', $users->fname, $users->lname,  $users->email, $users ->uname, $users->paswd);
+    mysqli_stmt_bind_param($stmt, 'ssssssi', $users->fname, $users->lname,  $users->email, $users->paswd, $users->descrip, $users ->uname, $users ->id);
     mysqli_stmt_execute($stmt);
     mysqli_close($yhteys);
-    print $json;
+    #print $json;
+    print "Thank you for signin up with us!";
 }
 catch(Exception $e){
     print "Tunnus jo olemassa tai muu virhe!";
@@ -42,8 +44,8 @@ function tarkistaJson($json){
     if (empty($json)){
         return false;
     }
-    $user=json_decode($json, false);
-    if (empty($users->fname  || $users->lname ||  $users->email || $users->uname || $users->paswd)){
+    $users=json_decode($json, false);
+    if (empty($users->fname  || $users->lname ||  $users->email || $users->paswd) ||  $users->uname){
         return false;
     }
     return $users;
