cf664c040ac9e967e88c5abdfd346c1232c443e8
jennanevalainen <90970239+jennanevalainen@users.noreply.github.com>
2022-02-17 14:37:31 +0200

    update

--- php/rekisterointi.php
+++ php/rekisterointi.php
@@ -1,37 +1,35 @@
+<?php 
 
-<?php
-$initials=parse_ini_file("../.ht.asetukset.ini");
+
 mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
-$json=isset($_POST["users"]) ? $_POST["users"] : "";
+$json=isset($_POST["user"]) ? $_POST["user"] : "";
 
-if (!($users=tarkistaJson($json))){
+if (!($user=tarkistaJson($json))){
     print "Täytä kaikki kentät";
-    exit;
-}
-
-try{
-   
-    $yhteys=mysqli_connect($initials["databaseserver"],
-        $initials["username"],
-        $initials["password"],
-        $initials["database"]
-        );
+    exit; 
     
+}
+session_start();
+$initials=parse_ini_file("../.ht.asetukset.ini");
+try {
+    $yhteys=mysqli_connect($initials["databaseserver"],
+                               $initials["username"],
+                               $initials["password"],
+                               $initials["database"]
+                                );
 } catch (Exception $e) {
     header("Location:../html/connectionError.html");
     exit;
 }
 
-//Tehdään sql-lause, jossa kysymysmerkeillä osoitetaan paikat
-//joihin laitetaan muuttujien arvoja
-$sql="insert into users(id, fname, lname, email, paswd, descrip, uname) values(?,?,?, SHA2(?, 256), ?, ?)";//sama kuin SHA2(?, 0)
+
+$sql="insert into users(fname, lname, email, paswd, uname) values(?,?,?, SHA2(?, 256),?)";//sama kuin SHA2(?, 0)
 try{
     $stmt=mysqli_prepare($yhteys, $sql);
-    mysqli_stmt_bind_param($stmt, 'ssssssi', $users->fname, $users->lname,  $users->email, $users->paswd, $users->descrip, $users ->uname, $users ->id);
+    mysqli_stmt_bind_param($stmt, 'sssss', $user->fname, $user->lname, $user->email, $user->paswd,$user->uname);
     mysqli_stmt_execute($stmt);
     mysqli_close($yhteys);
-    #print $json;
-    print "Thank you for signin up with us!";
+    print "Rekisteröinti onnistui";
 }
 catch(Exception $e){
     print "Tunnus jo olemassa tai muu virhe!";
@@ -44,10 +42,12 @@ function tarkistaJson($json){
     if (empty($json)){
         return false;
     }
-    $users=json_decode($json, false);
-    if (empty($users->fname  || $users->lname ||  $users->email || $users->paswd) ||  $users->uname){
+    $user=json_decode($json, false);
+    if (empty( $user->fname)|| empty($user->lname) ||empty($user->paswd) ||empty($user->uname)){
         return false;
     }
-    return $users;
+    return $user;
 }
 ?>
+
+
