404ec79d3567e9fa0a00f3b9fe7f7a4bb0196470
SanteriP <santeri.passi@gmail.com>
2022-02-22 10:20:51 +0200

    Remove unused script

--- php/profile.php
+++ php/profile.php
@@ -54,23 +54,6 @@ $picpath="$row->profpic";
           x.style.display = "none";
         }
       }
-      
-      function lahetaKayttaja(lomake){
-		var user=new Object();
-		user.tunnus=lomake.tunnus.value;
-		user.salasana=lomake.salasana.value;
-		var jsonUser=JSON.stringify(user);
-	
-		xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function() {
-	  		if (this.readyState == 4 && this.status == 200) {
-		    	document.getElementById("result").innerHTML = this.responseText;
-	  		}
-		};
-		xmlhttp.open("POST", "../php/rekisteroidyajax.php", true);
-		xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xmlhttp.send("user=" + jsonUser);	
-		}
     </script>
 </head>
 
